a7c72d90a43494f92287971605e158e325558aa4
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2019-01-12 16:35:19 +0530

    Update Authenticate.php

--- application/controllers/Authenticate.php
+++ application/controllers/Authenticate.php
@@ -14,6 +14,15 @@ class Authenticate extends CI_Controller {
     }
 
     public function login() {
+        isset($_POST['user_email']) ? isset($_POST['user_password']) ? '' : redirect('home') : redirect('home');
+        $auth = $this->auth->authenticate($_POST['user_email'], $_POST['user_password']);
+        if ($auth) {
+            $_SESSION['user_logged'] = TRUE;
+            $_SESSION['user_email'] = $_POST['user_email'];
+        }
+    }
+
+    public function signup() {
         if (isset($_POST['user_email']) && $_POST['user_password']) {
             $auth = $this->auth->login($_POST['user_email'], $_POST['user_password']);
             if ($auth) {
